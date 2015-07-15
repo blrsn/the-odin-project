c87b05d35a7f418b5a957b916b373bf959711323
larson004 <larson004@gmail.com>
2015-07-15 18:38:32 +0530

    completed ceasar cipher

--- Chap-03-Advanced-Ruby/Ruby_building_blocks/ceasar.rb
+++ Chap-03-Advanced-Ruby/Ruby_building_blocks/ceasar.rb
@@ -1,15 +1,66 @@
 def ceasar(str,shift)
   
-  #array = str.split("")
+  
+  # a - z is 97 - 122
+  # A - Z is 65 - 90
   
   str.length.times do |x|
     
-    str[x] = (str[x].ord + shift).chr
+    cipher = str[x].ord
     
-  end
+      case cipher
+        
+      when 65..90
+      
+      actual_shift =  shift
+      
+        while cipher != 90 && actual_shift != 0
+        
+        cipher += 1  
+        actual_shift -= 1
+        # puts cipher
+      
+        end
+        
+        actual_shift = actual_shift % 25
+        cipher = 64 + actual_shift if cipher == 90
+        
+        # puts "--#{cipher}"
+    
+        str[x] = cipher.chr 
+      
+      when 97..122
+    
+      actual_shift =  shift
+    
+        while cipher != 122 && actual_shift != 0
+      
+        cipher += 1  
+        actual_shift -= 1
+        
+        # puts cipher
+        end
+        
+        actual_shift = actual_shift % 25
+        cipher = 96 + actual_shift if cipher == 122
+    
+        # puts cipher
+        
+        str[x] = cipher.chr 
+        
+      else
+        "Do nothing"
+      
+      end
+          
+      
+    end 
   
-  str
-  
+    str
+    
 end
+
+puts ceasar("What a string!",5)
+puts ""
   
   
